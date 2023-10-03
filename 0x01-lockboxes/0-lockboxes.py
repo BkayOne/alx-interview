a4b5d801318e4f1f202a2b90e63a16443e919265
Frankelly Ozonwu <chiedoziefrankelly@gmail.com>
2023-10-03 23:16:16 +0000

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Lock boxes module"""
+
+
+def canUnlockAll(boxes):
+    """Check if all boxes to unlock all"""
+    n = len(boxes)
+    s_boxes = set([0])
+    u_boxes = set(boxes[0]).difference(set([0]))
+    while len(u_boxes) > 0:
+        box_idx = u_boxes.pop()
+        if not box_idx or box_idx >= n or box_idx < 0:
+            continue
+        if box_idx not in s_boxes:
+            u_boxes = u_boxes.union(boxes[box_idx])
+            s_boxes.add(box_idx)
+    return n == len(s_boxes)
