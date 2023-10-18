2b9404fee20fcf1916e08a84b4e9cf7cf91b55e7
Frankelly Ozonwu <chiedoziefrankelly@gmail.com>
2023-10-18 14:39:23 +0000

    Hot fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """log parsing module."""
 
+
 def processLine(line: str, status_codes: dict) -> int:
     """
     The line processing function.
@@ -11,12 +12,13 @@ def processLine(line: str, status_codes: dict) -> int:
         try:
             fsize = int(line.split()[-1])
             status = int(line.split()[-2])
-            if status in status_codes:
+            if status in status_codes.keys():
                 status_codes[status] += 1
             return fsize
         except ValueError:
             pass
     return 0
+
 
 def logLines(total_file_size: int, status_codes: dict):
     """Logs the processed lines to the standard output."""
@@ -26,29 +28,34 @@ def logLines(total_file_size: int, status_codes: dict):
         if num > 0:
             print(f'{code}: {num}', flush=True)
 
+
 def run():
     """The entrance function"""
     total_file_size = 0
     nlines = 0
     status_codes = {
-        200: 0,
-        301: 0,
-        400: 0,
-        401: 0,
-        403: 0,
-        404: 0,
-        405: 0,
-        500: 0
-    }
+            200: 0,
+            301: 0,
+            400: 0,
+            401: 0,
+            403: 0,
+            404: 0,
+            405: 0,
+            500: 0
+            }
     try:
         while True:
             line = input()
-            total_file_size += processLine(line, status_codes)
+            total_file_size += processLine(
+                    line,
+                    status_codes
+                    )
             nlines += 1
             if nlines % 10 == 0:
                 logLines(total_file_size, status_codes)
     except (KeyboardInterrupt, EOFError):
         logLines(total_file_size, status_codes)
+
 
 if __name__ == '__main__':
     run()
