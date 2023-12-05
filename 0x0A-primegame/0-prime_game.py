b4627f67fe74fe7b0d9360ad0ab5c2ce74b4d00f
Frankelly Ozonwu <chiedoziefrankelly@gmail.com>
2023-12-05 02:01:13 +0000

    Prime Game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Prime Game Algorithm Module"""
+
+
+def isWinner(x, nums):
+    """Check who is the winner of the prime game"""
+    ben = 0
+    maria = 0
+
+    # Based on pattern discovered, ben always wins at a step of 3
+    # Then wins the next which is 4.
+    # Then the pattern repeats itself.
+
+    for i in range(x):
+        if nums[i] == 1:
+            ben += 1
+        elif nums[i] % 4 == 0 or nums[i] % 4 == 3:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return 'Ben'
+    elif maria > ben:
+        return 'Maria'
+    else:
+        return None
